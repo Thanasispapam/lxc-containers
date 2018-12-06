84c30a97050c37f92fde3e808be84351ade25da3
thpap <thanasis.papamarinos@gmail.com>
2018-12-06 19:06:08 +0100

    Update rng.sh

--- rng.sh
+++ rng.sh
@@ -1,6 +1,7 @@
-#!/bin/bash
+#!/bin/ash
 
 dd if=/dev/random bs=4 count=16 status=none | od -A none -t u4
 
 
 ##shows random numbers
+##it is ash and not bash because in the alpine environment it s ash.
